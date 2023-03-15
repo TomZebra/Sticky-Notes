ddd66637323299a23aa1de4f245e4623283a3871
TomZebra <119517554+TomZebra@users.noreply.github.com>
2023-03-15 21:08:35 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@mui/icons-material/Highlight";
+import StickyNote2Icon from "@mui/icons-material/StickyNote2";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <StickyNote2Icon /> Sticky Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
